58cc71d12c97c2d1d9a6d83706cf8e874baffdd5
Yoana <yoanabarbie@icloud.com>
2024-08-06 16:53:19 +0200

    fix: another fix with h1 hero tag

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
-import { getImageUrl } from "../../utils"
-import styles from "./Hero.module.css"
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
 
 export const Hero = () => {
   return (
     <section className={styles.container}>
-        <div className={styles.content}>
-            <h1 className={styles.item}>Hi, I'm Yoana</h1>
-            <p className={styles.description}>I am a full-stack developer with 1 year of experience using React and NodeJS.
-                Reach out if you'd like to learn more!
-            </p>
-            <a className={styles.contactBtn} href="mailto:yoana.stamenovaa@gmail.com">Contact Me</a>
-        </div>
-        <img src={getImageUrl("hero/heroImage.png")} 
-        alt="Hero Image of me" 
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Yoana</h1>
+        <p className={styles.description}>
+          I'm a full-stack developer with 5 years of experience using React and
+          NodeJS. Reach out if you'd like to learn more!
+        </p>
+        <a href="mailto:myemail@email.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.png")}
+        alt="Hero image of me"
         className={styles.heroImg}
-        />
-            <div className={styles.topBlur}></div>
-            <div className={styles.bottomBlur}></div>
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
     </section>
-  )
-}
+  );
+};
