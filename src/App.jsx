8dbb19a34d17235fe87daa766e8a98af083c2b78
Yoana <yoanabarbie@icloud.com>
2024-08-06 13:57:35 +0200

    feat: navbar component finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import styles from './App.module.css'
+import { Navbar } from './components/Navbar/Navbar'
 
 function App() {
   return (
-    <>
-    <div className={styles.App}>Hello world!</div>
-    </>
+    <div className={styles.App}>
+      <Navbar />
+    </div>
   )
 }
 
