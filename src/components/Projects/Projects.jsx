00a59a6d10a6a61b2886c7f183564f581261096e
Yoana <yoanabarbie@icloud.com>
2024-08-07 10:41:53 +0200

    feat: Projects finished successfully

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,28 +1,19 @@
-import React from 'react'
+import React from "react";
 
-import projects from "../../data/projects.json"
+import styles from "./Projects.module.css";
+
+import projects from "../../data/projects.json";
+import { ProjectCard } from "./ProjectCard";
 
 export const Projects = () => {
-    return (
-      <section>
-        <h2>Projects</h2>
-        <div>
-          {projects.map((project, id) => {
-            return ( 
-              <div key={id}>
-                <img src={project.imageSrc} alt={`Image of ${project.title}`} />
-                <h3>{project.title}</h3>
-                <p>{project.description}</p>
-                <ul>
-                  {project.skills.map((skill, id) => {
-                    return <li key={id}>{skill}</li>; 
-                  })}
-                </ul>
-              </div> 
-            );
-          })}
-        </div>
-      </section>
-    )
-  }
-  
+  return (
+    <section className={styles.container} id="Projects">
+      <h2 className={styles.title}>Projects</h2>
+      <div className={styles.projects}>
+        {projects.map((project, id) => {
+          return <ProjectCard key={id} project={project} />;
+        })}
+      </div>
+    </section>
+  );
+};
